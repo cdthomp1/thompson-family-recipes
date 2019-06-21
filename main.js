b35a72ecf2083ba270c26c7dbfe55d76d9ed5fe4
Cameron Thompson <tho17015@byui.edu>
2019-06-21 09:35:23 -0600

    added

--- main.js
+++ main.js
@@ -186,6 +186,29 @@ function chickenRanchWraps() {
       xhttp.send();
 }
 
+function creamCheesandChickenTaquitos() {
+  let url = "https://cdthomp1.github.io/what-can-I-make/recipes/cream-cheese-and-chicken-taquitos-r.json"
+    
+    var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          let recipe = JSON.parse(this.responseText)
+    
+          document.getElementById(`recipe-name`).innerHTML = recipe.title;
+    
+            recipe.ingredients.forEach(ingredient =>{
+                addItems(`${ingredient.amount}   ${ingredient.ingredient}`, 'recipe-ingredient-list')
+            })
+    
+            recipe.directions.forEach(direction =>{
+                addItems(direction.process, 'recipe-direction-list')
+            })  
+        }
+      };
+      xhttp.open("GET", url, true);
+      xhttp.send();
+}
+
     function addItems(item,list){
     
     var ul = document.getElementById(list);
